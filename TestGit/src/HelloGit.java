6d1af95990ca8fa905327ecad0e9f1b1aea90949
Takeshi Masaki <m.t.bb1924@gmail.com>
2021-03-11 21:26:25 +0900

    first commit

--- TestGit/src/HelloGit.java
+++ TestGit/src/HelloGit.java
@@ -4,7 +4,7 @@ public class HelloGit {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
-		System.out.println("Hello Git!");
+		System.out.println("Hello Git! ver2");
 	}
 
 }
